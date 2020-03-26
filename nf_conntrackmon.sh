f9981a9e8dc049feba5c22f5003ec9503637a998
Jesus Saavedra Jr <34646811+combathero32@users.noreply.github.com>
2020-03-27 03:48:06 +0800

    Create nf_conntrackmon.sh

---
+++ nf_conntrackmon.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+threshhold_pct=50
+nf_conntrack_max=16384
+
+nf_conntrack_count=$(cat /proc/net/nf_conntrack | wc | awk '{print $1}')
+nf_conntrack_dec=$(echo "scale=2 ; $nf_conntrack_count / $nf_conntrack_max" | bc)
+nf_conntrack_pctdec=$(expr $nf_conntrack_dec*100 | bc)
+nf_conntrack_pct=$(echo ${nf_conntrack_pctdec%.*})
+
+if [ $nf_conntrack_pct -gt $threshhold_pct ]; then
+	d=$(date)
+        echo "To:email@example.com" > /etc/nf_conntrackmon/msgfinal.txt
+        echo "From:email@example.com" >> /etc/nf_conntrackmon/msgfinal.txt
+        echo "Subject:Cron Alert (server notification: nf_conntrack count $nf_conntrack_pct% too high) $d" >> /etc/nf_conntrackmon/msgfinal.txt
+        echo "" >> /etc/nf_conntrackmon/msgfinal.txt
+        uptime >> /etc/nf_conntrackmon/msgfinal.txt
+        echo "" >> /etc/nf_conntrackmon/msgfinal.txt
+
+        ssmtp email@example.com < /etc/nf_conntrackmon/msgfinal.txt
+
+fi
